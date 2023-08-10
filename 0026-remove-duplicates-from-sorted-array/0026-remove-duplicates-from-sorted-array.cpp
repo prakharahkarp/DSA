aedf76ab80e855beefc8cb15102eb68b64104021
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-08-10 20:23:10 +0530

    Time: 7 ms (90.72%), Space: 18.3 MB (56.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,6 @@ public:
 };
 
 // approach 1 use hash map
+
+// approach 2 we can use the fact that the array is sorted and hence whenever we encounter a condition wherein nums[i]!=nums[i-1], it means that we will no longer see nums[i-1] and hence we can keep this nums[i-1] as a unique element
+// keeping the last element of array always helps in certain edge case such as when there is only one element in the array or in case wherein, we dont encounter the condition nums[i]!=nums[i-1] for the last element either due to duplicacy or without any reason
