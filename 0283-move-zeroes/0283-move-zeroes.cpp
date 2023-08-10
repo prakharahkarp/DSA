9c8b3adea67b51f8edc7d133fb340fa632b2c490
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-08-10 13:29:14 +0530

    Time: 14 ms (95.67%), Space: 19.1 MB (70.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -14,10 +14,7 @@ public:
                 nonzeroPos++;
                 }
             }
-        // for(;nonzeroPos<nums.size();nonzeroPos++){
-        //     nums[nonzeroPos]=0;
-        // }
     }
 };
 
-// approach 2 is to use a pointer that points to places where non-zero elements must reside and whenever we find a non-zero element, we move it to this position and increase the pos value by 1. In the end we just make all remaining elemnets 0
+// approach 2 is to use a pointer that points to places where non-zero elements must reside and whenever we find a non-zero element, we move it to this position and increase the pos value by 1.
