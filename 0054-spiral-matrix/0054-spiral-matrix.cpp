5675ee040986ea9f876e5c38aa135fecf6e99b0d
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-08-20 04:55:48 +0530

    Time: 3 ms (36.58%), Space: 7 MB (23.71%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,3 +1,7 @@
+// We have to print a total of m*n elements in a spiral order going from outside to inside
+// we print these elements in rounds and in each round we move right->down->left->up until countEle>=m*n
+// we also maintain a count of these rounds
+
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
@@ -9,25 +13,21 @@ public:
 //             traverse right
             for(int i=countRound;i<n-countRound;i++){
                 ans[countEle++]=matrix[countRound][i];
-                cout<<matrix[countRound][i]<<" ";
             }
             if(countEle>=m*n) break;
 //             traverse down
             for(int i=countRound+1;i<m-countRound;i++){
                 ans[countEle++]=matrix[i][n-1-countRound];
-                cout<<matrix[i][n-1-countRound]<<" ";
             }
             if(countEle>=m*n) break;
 //             traverse left
             for(int i=n-countRound-2;i>=countRound;i--){
                 ans[countEle++]=matrix[m-1-countRound][i];
-                cout<<matrix[m-1-countRound][i]<<" ";
             }
             if(countEle>=m*n) break;
 //             traverse up
             for(int i=m-countRound-2;i>=countRound+1;i--){
                 ans[countEle++]=matrix[i][countRound];
-                cout<<matrix[i][countRound]<<" ";
             }
             countRound++;
         }
