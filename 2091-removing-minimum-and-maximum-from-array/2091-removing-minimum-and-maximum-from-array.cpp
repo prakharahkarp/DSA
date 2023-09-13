ffd99a6fb8b15457397933d9f54f29e427a5755b
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-09-14 02:35:15 +0530

    Time: 128 ms (53.69%), Space: 88.7 MB (5.53%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int minEle = nums[0], maxEle = nums[0], minElePos = 0, maxElePos = 0, n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] > maxEle){
+                maxEle = nums[i];
+                maxElePos = i;
+            }
+            if(nums[i] < minEle){
+                minEle = nums[i];
+                minElePos = i;
+            }
+        }
+        int delOpp = 0, delSame1 = 0, delSame2;
+        
+//         when the two elements to be deleted are nearer to the opposite ends
+        delOpp += min(minElePos+1, n - minElePos) + min(maxElePos+1, n - maxElePos);
+        
+//         when the two elements to be deleted are nearer to one another and hence the deletion take place from one end only
+        if(minElePos < maxElePos) delSame1 = maxElePos + 1;
+        else delSame1 = minElePos + 1;
+        if(n - minElePos < n - maxElePos) delSame2 = n - maxElePos;
+        else delSame2 = n - minElePos;
+        cout << delOpp << " " << delSame1 << " " << delSame2 << "\n";
+        return min({delOpp, delSame1, delSame2});
+    }
+};
