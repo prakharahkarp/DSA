8983cb9f69aabe870998f77bf838596dd46e126d
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-09-24 21:15:56 +0530

    Time: 181 ms (55.65%), Space: 118.5 MB (42.04%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverse(ListNode *prev, ListNode *curr){
+        ListNode *next = curr->next;
+        while(curr != nullptr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return true;
+        
+        ListNode *slow = head, *fast = head, *prev = nullptr;
+        int flag = 0;
+        
+//         finding the middle node of the LL
+        while(fast != nullptr && fast->next != nullptr){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // when the nodes are odd, the fast pointer points to the last node and when they are even and otherwise it points to null
+//         in case of odd number of nodes, the middle node has no one to compare with, so we can just ignore it
+        if(fast != nullptr){
+            slow = slow->next;
+        }
+        
+//         reversing the later half of the LL, so that we can compare the ith node from the beginning and end easily
+        ListNode *revHead = reverse(nullptr, slow);
+        ListNode* curr = head, *revCurr = revHead;
+        
+        // comparing the ith node from the beginning and end
+        while(revCurr != nullptr){
+            if(curr->val != revCurr->val){
+                flag = 1;
+                break;
+            }
+            revCurr = revCurr->next;
+            curr = curr->next;
+        }
+        
+//         restoring the original form of the Linked List
+        revHead = reverse(nullptr, revHead);
+        prev->next = revHead;
+        
+        return flag == 0;
+    }
+};
