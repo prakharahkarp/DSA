1db8d97c4b400882ab925be9e00c55e8ba7d2eaf
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-09-26 00:37:48 +0530

    Time: 3 ms (96.21%), Space: 11.6 MB (15.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,30 +17,37 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node *dummyNode = new Node(0);
-        Node *temp = head, *curr = dummyNode;
-        unordered_map<Node*, Node*> rand_mapping;
+        if(head == nullptr) return head;
         
-        while(temp != nullptr){
-            curr->next = new Node(temp->val);
-            rand_mapping[temp] = curr->next;
-            curr = curr->next;
-            temp = temp->next;
+        Node *curr = head, *temp = nullptr;
+//         creating and linking nodes from old list and their respective shallow copies
+        while(curr != nullptr){
+            temp = new Node(curr->val);
+            temp->next = curr->next;
+            curr->next = temp;
+            curr = curr->next->next;
         }
-        
-        temp = head, curr = dummyNode->next;
-        while(temp != nullptr){
-            if(temp->random != nullptr){
-                curr->random = rand_mapping[temp->random];
+//         creating random pointers for new list
+        curr = head;
+        while(curr != nullptr){
+            if(curr->random != nullptr){
+                curr->next->random = curr->random->next;
             }
+            curr = curr->next->next;
+        }
+//         seperating the two lists
+        Node *newHead = head->next;
+        temp = newHead, curr = head;
+        
+        curr->next = curr->next->next;
+        curr = curr->next;
+        while(curr != nullptr){
+            temp->next = curr->next;
+            curr->next = curr->next->next;
             curr = curr->next;
             temp = temp->next;
         }
         
-        temp = dummyNode;
-        dummyNode = dummyNode->next;
-        delete temp;
-        
-        return dummyNode;
+        return newHead;
     }
 };
