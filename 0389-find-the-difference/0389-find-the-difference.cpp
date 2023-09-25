56a888f02f8feea40f2a06c7793334aec91dd6f7
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-09-26 03:52:53 +0530

    Time: 4 ms (47.89%), Space: 7.1 MB (5.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -4,10 +4,9 @@ public:
         int totalXOR = 0;
         for(int i = 0; i < s.size(); i++){
             totalXOR = (s[i]-'a')^totalXOR;
-        }
-        for(int i = 0; i < t.size(); i++){
             totalXOR = (t[i]-'a')^totalXOR;
         }
+        totalXOR = (t[t.size()-1]-'a')^totalXOR;
         return char(97+totalXOR);
     }
 };
