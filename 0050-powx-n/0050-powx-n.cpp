cd422a5804cc50c3476e82e759bc9d7bcfc95383
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-01 09:57:12 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (13.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    double findVal(double x, int n){
+        if(n == 0) return 1;
+        if(n % 2) return x * findVal(x * x, n / 2);
+        return findVal(x * x, n / 2);
+    }
+    
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(x == 1 || n == 0) return 1;
+        double tempRes;
+        if(n == INT_MIN){
+            tempRes = findVal(x, INT_MAX) * x;
+            tempRes = 1.0 / tempRes;
+        }
+        else if(n < 0){
+            tempRes = findVal(x, abs(n));
+            tempRes = 1/ tempRes;
+        }
+        else tempRes = findVal(x, n);
+        
+        return tempRes;
     }
 };
+
+// 2 4 16 256 
+// 1 2 4   8
