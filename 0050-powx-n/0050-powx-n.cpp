feb7af4591d1345786e45e17ce223a2fc898106a
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-01 11:02:07 +0530

    Time: 3 ms (42.69%), Space: 6.3 MB (26.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    double findVal(double x, int n){
-        if(n == 0) return 1;
-        if(n % 2) return x * findVal(x * x, n / 2);
-        return findVal(x * x, n / 2);
-    }
-    
     double myPow(double x, int n) {
         if(x == 1 || n == 0) return 1;
-        double tempRes;
+        double res = 1;
         if(n == INT_MIN){
-            tempRes = findVal(x, INT_MAX) * x;
-            tempRes = 1.0 / tempRes;
+            res = x;
+            n++;
         }
-        else if(n < 0){
-            tempRes = findVal(x, abs(n));
-            tempRes = 1/ tempRes;
+        int flag = 0;
+        if(n < 0){
+            flag = 1;
+            n = 0 - n;
         }
-        else tempRes = findVal(x, n);
         
-        return tempRes;
+        while(n > 0){
+            if(n % 2){
+                res *= x;
+                n--;
+            }
+            x *= x;
+            n /= 2;
+        }
+        if(flag) return 1/res;
+        return res;
     }
 };
 
-// 2 4 16 256 
-// 1 2 4   8
