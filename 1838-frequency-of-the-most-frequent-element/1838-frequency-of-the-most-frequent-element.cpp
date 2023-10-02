f5c37785d1b16a0deb0efbb79e57dccd37b525b5
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-02 17:30:40 +0530

    Time: 268 ms (5.53%), Space: 99.3 MB (24.32%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isGood(int x, vector<int>& nums, int k){
+        long long windowStart = 0, windowEnd = x, windowSum = 0, minWindowSum = 0, maxEle = nums[windowEnd - 1];
+        for(int i = 0; i < windowEnd; i++){
+            windowSum += maxEle - nums[i];
+        }
+        minWindowSum = windowSum;
+        for(int i = windowEnd; i < nums.size(); i++){
+            windowSum += (nums[i] - maxEle) * (x - 1) - (maxEle - nums[windowStart++]);
+            maxEle = nums[i];
+            minWindowSum = min(windowSum, minWindowSum);
+        }
+        return minWindowSum <= k;
+    }
+    
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        // vector<int> diff(nums.size(), 0);
+        // for(int i = 1; i < nums.size(); i++){
+        //     diff[i] = nums[i] - nums[i - 1];
+        // }
+        // for(int i = 0; i < diff.size(); i++){
+        //     cout<<diff[i]<<" ";
+        // }
+        // cout<<"\n";
+        int l = 1;
+        int r = nums.size() + 1;
+        while(r > l + 1){
+            int mid = l + (r - l)/2;
+            if(isGood(mid, nums, k)) l = mid;
+            else r = mid;
+        }
+        return l;
+    }
+};
