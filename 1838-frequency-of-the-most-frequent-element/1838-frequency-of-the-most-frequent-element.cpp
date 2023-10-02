c7d1699c42384c47cf57e6acbe8d0cc9dc1a7ee0
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-03 01:51:28 +0530

    Time: 200 ms (12.64%), Space: 99.4 MB (16.12%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool isGood(int x, vector<int>& nums, int k){
-        long long windowStart = 0, windowEnd = x, windowSum = 0, minWindowSum = 0, maxEle = nums[windowEnd - 1];
-        for(int i = 0; i < windowEnd; i++){
-            windowSum += maxEle - nums[i];
-        }
-        minWindowSum = windowSum;
-        for(int i = windowEnd; i < nums.size(); i++){
-            windowSum += (nums[i] - maxEle) * (x - 1) - (maxEle - nums[windowStart++]);
-            maxEle = nums[i];
-            minWindowSum = min(windowSum, minWindowSum);
-        }
-        return minWindowSum <= k;
-    }
-    
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int l = 0;
-        int r = nums.size() + 1;
-        while(r > l + 1){
-            int mid = l + (r - l)/2;
-            if(isGood(mid, nums, k)) l = mid;
-            else r = mid;
+        long long windowStart = 0, windowSize = 1, maxWindowSize = 1, curr = 1;
+        long long cost = 0;
+        while(curr < nums.size()){
+            if(cost + (nums[curr] - nums[curr - 1]) * (curr - windowStart) <= k){
+                cost += (nums[curr] - nums[curr - 1]) * (curr - windowStart);
+                windowSize++;
+                maxWindowSize = max(windowSize, maxWindowSize);
+                curr++;
+            } else if(windowStart < curr){
+                cost -= (nums[curr - 1] - nums[windowStart++]);
+                windowSize--;
+            } else{
+                curr++;
+                windowSize++;
+            }
         }
-        return l;
+        return maxWindowSize;
     }
 };
