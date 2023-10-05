6e8bd5132789500c2f6a9f98ff578d08666c1863
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-06 02:58:43 +0530

    Time: 378 ms (65.37%), Space: 140.8 MB (55.90%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -3,9 +3,7 @@ public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
         unordered_map<double, int> wid_ht_ratio;
         for(int i = 0; i < rectangles.size(); i++){
-            int width = rectangles[i][0];
-            int height = rectangles[i][1];
-            double temp = (double)width / height;
+            double temp = (double)rectangles[i][0] / rectangles[i][1];
             wid_ht_ratio[temp]++;
         }
         long long total_pairs = 0;
