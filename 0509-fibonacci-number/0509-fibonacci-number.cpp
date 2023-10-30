d77b15a728b46f7147d5ba4a4a09cd85d22144f4
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-10-30 20:37:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (33.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
+     int findNthTerm(int n, vector<int>& cache){
+        if(cache[n] != -1) return cache[n];
+        int val = findNthTerm(n - 2, cache) + findNthTerm(n - 1, cache);
+        cache[n] = val;
+        return cache[n];
+    }
+    
     int fib(int n) {
         if(n < 2) return n;
-        return fib(n - 2) + fib(n - 1);
+        vector<int> cache(31, -1);
+        cache[0] = 0;
+        cache[1] = 1;
+        return findNthTerm(n, cache);
     }
 };
