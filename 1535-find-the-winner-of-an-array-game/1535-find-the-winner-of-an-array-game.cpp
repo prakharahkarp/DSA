8d41fbf3a231732ddc70ffbd9d6d2c9bf989500c
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-08 23:58:08 +0530

    Time: 99 ms (13.83%), Space: 63.5 MB (56.88%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
+        // why this approach works is because when the loop breaks we will have a winner
+//         the winner would have already won 'k' rounds 
+//         and if the number of rounds won is less than k, then we would have reached the end of the array and at this point , we would have the largest number in the array and since all elements in our array are distinct, therefore our winner will win all further rounds(and hence will win k consecutive rounds at some point) and hence we don't need to shift the loser to the end of the array.
         int curr = 1, winner = arr[0], consecCount = 0, prevWinner = arr[0];
         while(curr < arr.size()){
-            if(arr[curr] > winner){
-                prevWinner = winner;
-                winner = arr[curr];
-                consecCount = 1;
-            } else {
-                consecCount++;
-            }
+            prevWinner = winner;
+            winner = max(winner, arr[curr]);
+            if(winner == prevWinner) consecCount++;
+            else consecCount = 1;
             if(consecCount >= k) break;
-            cout << curr << " " << winner << "\n";
             curr++;
         }
         return winner;
