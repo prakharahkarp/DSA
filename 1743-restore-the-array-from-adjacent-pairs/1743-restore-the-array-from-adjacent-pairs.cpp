103190f2938e785daeddbd9aa3a724fd43197b50
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-11 02:00:36 +0530

    Time: 467 ms (43.20%), Space: 120.6 MB (61.93%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -18,26 +18,36 @@ public:
     }
     
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        
         int n = adjacentPairs.size();
         unordered_map<int, vector<int>> mp;
         unordered_map<int, int> visited;
         int src;
+        
 //         creating an adjacency matrix for our pairs
+//         since our vertices can be negative, therefore we use a map
         for(int i = 0; i < adjacentPairs.size(); i++){
             mp[adjacentPairs[i][1]].push_back(adjacentPairs[i][0]);
             mp[adjacentPairs[i][0]].push_back(adjacentPairs[i][1]);
+//             making sure that initially all of our vertices are unvisited
             visited[adjacentPairs[i][1]] = 0;
             visited[adjacentPairs[i][0]] = 0;
         }
 //         finding the source vertex i.e. the first element of the array
+//         only the end elements will be linked to only one element, all other elements will be linked to multiple elements
+//         so we find one of the two ending elements and begin our traversal from there
         for(auto it : mp){
             if(it.second.size() == 1){
                 src = it.first;
                 break;
             }
         }
+        
         vector<int> ans;
+        
         BFS(src, mp, n + 2, visited, ans);
+        
+        
         return ans;
     }
 };
