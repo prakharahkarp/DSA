e308c0058f21d429fe6162e6905fd8ee3071cbc1
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-14 18:53:30 +0530

    Time: 274 ms (62.10%), Space: 266 MB (5.02%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<pair<int, int>> occurrence(26, {-1, -1});
         
+        vector<pair<int, int>> occurrence(26, {-1, -1});
+//         storing the first and last occurrence for each character
         for(char ch = 'a'; ch <= 'z'; ch++){
             for(int i = 0; i < s.length(); i++){
                 if(s[i] == ch){
@@ -18,13 +19,17 @@ public:
             }
         }
         
+//         precomputing frequency of each english alphabet character present in the string uptil that index
         vector<vector<int>> frequencies;
         vector<int> temp(26, 0);
         for(int i = 0; i < s.length(); i++){
             temp[s[i] - 'a']++;
             frequencies.push_back(temp);
         }
-
+        
+//         traversing from a-z, finding the number of unique characters present between the first and last occurrence of that character in the string
+//         i could have used a set so i dont have to precompute anything, i could have just traversed between the first and last occurrence of each character, storing every character in the set and at the end the size of the set would have been the number of unique characters in between
+//         but i thought that this approach would be O(N^2) as i forgot that i would be traversing over the entire string in the outer loop also but in reality, the outer loop just runs from a-z, and hence the TC would have been O(26*N)
         int totalPallindromicSubsequences = 0;
         for(char ch = 'a'; ch <= 'z'; ch++){
             int firstOccurrence = occurrence[ch - 'a'].first;
@@ -37,7 +42,11 @@ public:
                 if(diff_freq > 0) uniqueCharactersBetween++;
             }
             totalPallindromicSubsequences += uniqueCharactersBetween;
-        }
+        } // TC of this loop O(26 * 26)
+        
        return totalPallindromicSubsequences;     
     }
 };
+
+// SC -> O(26 * N)
+// TC -> O(N)
