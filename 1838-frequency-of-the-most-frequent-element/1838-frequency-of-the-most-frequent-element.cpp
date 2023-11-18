39355eb16caf3f501e7f9c3d58fd6d25d3f2116e
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-18 22:06:48 +0530

    Time: 173 ms (48.09%), Space: 99.4 MB (43.97%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        long long windowStart = 0, freq = 0, maxFreq = 0, curr = 0, cost = 0;
-
+        
+        long long windowStart = 0, freq = 1, maxFreq = 1, curr = 1, cost = 0;
+        
         while(curr < nums.size()){
             if(cost + freq * (nums[curr] - nums[max(curr - 1, (long long)0)]) <= k){
                 cost += freq * (nums[curr] - nums[max(curr - 1, (long long)0)]);
                 freq++;
                 curr++;
                 maxFreq = max(maxFreq, freq);
-            } else if(windowStart < curr){
+            } else if(cost > 0){
                 cost -= (nums[curr - 1] - nums[windowStart]);
                 windowStart++;
                 freq--;
