21700a19f8d76f0f5f0ca61132c1a9591e03d677
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-18 22:03:40 +0530

    Time: 177 ms (35.62%), Space: 99.5 MB (13.51%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        long long windowStart = 0, windowSize = 1, maxWindowSize = 1, curr = 1, cost = 0;
+        long long windowStart = 0, freq = 1, maxFreq = 1, curr = 1, cost = 0;
+
         while(curr < nums.size()){
-            if(cost + (nums[curr] - nums[curr - 1]) * (curr - windowStart) <= k){
-                cost += (nums[curr] - nums[curr - 1]) * (curr - windowStart);
-                windowSize++;
-                maxWindowSize = max(windowSize, maxWindowSize);
+            if(cost + freq * (nums[curr] - nums[curr - 1]) <= k){
+                cost += freq * (nums[curr] - nums[curr - 1]);
+                freq++;
                 curr++;
+                maxFreq = max(maxFreq, freq);
             } else if(windowStart < curr){
-                cost -= (nums[curr - 1] - nums[windowStart++]);
-                windowSize--;
-            } else{
+                cost -= (nums[curr - 1] - nums[windowStart]);
+                windowStart++;
+                freq--;
+            } else {
                 curr++;
-                windowSize++;
+                windowStart++;
             }
         }
-        return maxWindowSize;
+        return maxFreq;
     }
 };
