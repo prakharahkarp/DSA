112167e056a831dc9198ff8c3064b6cc7b266597
prakharahkarp <135894233+prakharahkarp@users.noreply.github.com>
2023-11-21 00:59:54 +0530

    Time: 202 ms (12.23%), Space: 102.6 MB (19.78%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findTime(vector<string>& garbage, vector<int>& travel, char ch){
+        
         int totalTime = 0, pickTime = 0, travelTime = 0, houseInd = 0;
         // cout << ch << "\n";
         for(auto waste : garbage){
@@ -12,10 +13,12 @@ public:
             }
             
             totalTime += pickTime;
+            
             if(pickTime){
                 totalTime += travelTime;
                 travelTime = 0;
             }
+            
             if(houseInd == travel.size()) break;
             travelTime += travel[houseInd++];
             // cout << totalTime << " " << pickTime << " " << travelTime << "\n";
@@ -25,6 +28,7 @@ public:
     }
     
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        
         return findTime(garbage, travel, 'G') + findTime(garbage, travel, 'M') + findTime(garbage, travel, 'P');
     }
 };
